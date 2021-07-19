d02a4377323fda7fb01f287b2c885f27f994c948
mzerbinatti <michel.zerbinati@hotmail.com>
2021-07-19 00:59:50 -0300

    Simplificando a API. Retornando apenas o valor.

--- v1/nodejs/api/controllers/passagensAereas.js
+++ v1/nodejs/api/controllers/passagensAereas.js
@@ -1,22 +1,12 @@
-module.exports = () => {
-    const controller = {};
 
+module.exports =  () =>  {
+    
     // Simula um valor de Passagem Aérea
     let valorMin = 200;
     let valorMax = 5000;
-    let valor = Number((Math.random() * (valorMax - valorMin) + valorMin).toFixed(2));
 
-    // Simula um id ded Cidade
-    let id_cidade = Math.random().toString(36).replace('0.', '');
-
-    // Forma o Json a ser retornado na requisição
-    let jsonRetorno = 
-    {
-        id_cidade: id_cidade,
-        valor: valor
-    };
-
-    controller.listarPassagensAereas = (req, res) => res.status(200).json(jsonRetorno);
+    const controller = {};
+    controller.listarPassagensAereas = async (req, res) => res.status(200).json( Number((Math.random() * (valorMax - valorMin) + valorMin).toFixed(2)) );
 
     return controller;
 }
